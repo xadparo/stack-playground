87c9dcbd3835c0cbeb54190e79b5bad143dcc63c
xadparo <45482148+xadparo@users.noreply.github.com>
2022-07-05 16:58:04 +0900

    feat: nest cli template setting(+ts, lint, test)

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import 'module-alias/register'
+import 'reflect-metadata'
+import 'source-map-support/register'
+
+import { AppModule } from '@/app.module'
+import { NestFactory } from '@nestjs/core'
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+  await app.listen(3000)
+}
+bootstrap()
